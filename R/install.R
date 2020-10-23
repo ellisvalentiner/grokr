a6a115e84e79f4775e1a030c38fa76d55449064a
Ellis Valentiner <ellisvalentiner@gmail.com>
2020-10-23 12:53:21 -0400

    fix: install pygrok in github actions

--- R/install.R
+++ R/install.R
@@ -5,10 +5,13 @@
 #'
 #' @export install_pygrok
 #'
+#' @return Invisible `NULL`, called for side effects
+#'
 #' @examples
 #' \dontrun{
 #' install_pygrok()
 #' }
 install_pygrok <- function(method = "auto", conda = "auto") {
   reticulate::py_install("pygrok", method = method, conda = conda)
+  invisible(NULL)
 }
