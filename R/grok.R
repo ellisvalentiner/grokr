e602e981436ff129a4bd09d6b503c537e764149a
Ellis Valentiner <ellisvalentiner@gmail.com>
2020-10-23 22:11:27 -0400

    feat: add grok_match

--- R/grok.R
+++ R/grok.R
@@ -11,3 +11,16 @@ grok <- function(pattern="", custom_patterns_dir=NULL, custom_patterns_list=reti
   pygrok$Grok(pattern, custom_patterns_dir, custom_patterns_list)
 }
 
+#' grok_match
+#'
+#' @param string input vector. Either a character vector, or something coercible to one.
+#' @param pattern the pattern to be matched
+#' @param custom_patterns_dir used to pass custom patterns
+#' @param custom_patterns_list used to pass custom patterns
+#'
+#' @return
+#' @export grok_match
+grok_match <- function(string, pattern="", custom_patterns_dir=NULL, custom_patterns_list=reticulate::dict()) {
+  g <- pygrok$Grok(pattern, custom_patterns_dir, custom_patterns_list)
+  purrr::map(string, g$match)
+}
