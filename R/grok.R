7ef99247cfe1b7e98cfbe8f799b386eece8c6b7b
Ellis Valentiner <ellisvalentiner@users.noreply.github.com>
2020-10-24 15:58:20 -0400

    feat: add grok_match (#3)
    * feat: add grok_match
    
    * docs: update and attributions
    
    * test: add test for grok_match

--- R/grok.R
+++ R/grok.R
@@ -11,3 +11,17 @@ grok <- function(pattern="", custom_patterns_dir=NULL, custom_patterns_list=reti
   pygrok$Grok(pattern, custom_patterns_dir, custom_patterns_list)
 }
 
+#' grok_match
+#'
+#' @param string input vector. Either a character vector, or something coercible to one.
+#' @param pattern the pattern to be matched
+#' @param custom_patterns_dir used to pass custom patterns
+#' @param custom_patterns_list used to pass custom patterns
+#'
+#' @return list with variable names specified in pattern and their corresponding values
+#'
+#' @export grok_match
+grok_match <- function(string, pattern="", custom_patterns_dir=NULL, custom_patterns_list=reticulate::dict()) {
+  g <- pygrok$Grok(pattern, custom_patterns_dir, custom_patterns_list)
+  purrr::map(string, g$match)
+}
--- tests/testthat/test-patterns.R
+++ tests/testthat/test-patterns.R
@@ -2,3 +2,8 @@ test_that("pattern matching works", {
   g <- grok("Hello %{WORD:name}")
   expect_identical(g$match("Hello world!"), list("name"="world"))
 })
+
+
+test_that("pattern matching works", {
+  expect_identical(grok_match("Hello world!", "Hello %{WORD:name}"), list(list("name"="world")))
+})
